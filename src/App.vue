d91dfe20eaf397b0f4c6a372ca8af7082b0e00fd
Eugen-Alexander Habrony <eugen-alexander.habrony@ho2-gmbh.de>
2021-01-04 20:02:39 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="container">
+    <h2>{{ userName }}</h2>
+    <h3>{{ age }}</h3>
+  </section>
+</template>
+
+<script>
+import {ref} from 'vue';
+
+export default {
+  setup() {
+    const uName = ref('Maximilian');
+    const uAge = ref(31);
+
+    setTimeout(function () {
+      uName.value = 'Max';
+    }, 2000);
+
+    return {
+      userName: uName,
+      age: uAge
+    }
+  }
+  // data() {
+  //   return {
+  //     userName: 'Maximilian',
+  //   };
+  // },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+.container {
+  margin: 3rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  text-align: center;
+}
+</style>
